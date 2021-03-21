b1e90235c5efd3f2a743bf0e6c3a2f0586c05416
devopsguys3 <70590333+devopsguys3@users.noreply.github.com>
2021-03-21 08:36:06 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ package com.chakilams;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World prasanna kumar");
+        System.out.println("Hello World santhosh kumar");
     }
 }
