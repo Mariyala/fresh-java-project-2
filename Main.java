9358548f45e93b32f59cbb6993a8514f7dcc41c0
Mariyala <m.sai23905@gmail.com>
2021-06-01 18:45:25 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ package com.chakilams;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World sai-1");
+        System.out.println("Hello World sai-2");
     }
 }
